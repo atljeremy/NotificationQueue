b42d3f74ea419618f12b9397bf8a845ff234f590
Jeremy Fox <atljeremy@me.com>
2017-03-23 09:27:49 -0400

    Created new project with cocoa touch framework target for use with Carthage

---
+++ NotificationQueue/NotificationQueue.h
@@ -0,0 +1,19 @@
+//
+//  NotificationQueue.h
+//  NotificationQueue
+//
+//  Created by Jeremy Fox on 3/23/17.
+//  Copyright © 2017 Jeremy Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NotificationQueue.
+FOUNDATION_EXPORT double NotificationQueueVersionNumber;
+
+//! Project version string for NotificationQueue.
+FOUNDATION_EXPORT const unsigned char NotificationQueueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NotificationQueue/PublicHeader.h>
+
+
